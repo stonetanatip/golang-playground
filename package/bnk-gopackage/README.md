335e890352e967f3b07f43431fa66eaa8b7020d6
Tanatip Lueang-on <tanatip@odds.team>
2019-12-18 11:31:16 +0700

    add BNK package

---
+++ package/bnk-gopackage/README.md
@@ -0,0 +1,2 @@
+# bnk-gopackage
+อันนี้สร้างขึ้นมาเพื่อ ใช้ในเป็น Example Package สำหรับการเขียน Blog Medium ตอนที่ 4 เรื่องการใช้งาน Package ของภาษา Go
---
+++ package/bnk-gopackage/bnkgopackage.go
@@ -0,0 +1,7 @@
+package bnkgopackage
+
+var MemberLists = [3]string{"เฌอปราง", "เจนนิษฐ์", "ไข่มุก"}
+
+func GetFullNameCherprang() string {
+	return "เฌอปราง อารีย์กุล"
+}
